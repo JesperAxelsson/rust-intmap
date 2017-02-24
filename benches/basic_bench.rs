07b4ac7cd770c93b503f3794b19eda375aa94b19
Jesper Axelsson <jesperaxe@gmail.com>
2017-02-25 00:18:08 +0100

    Updated benchmarks

--- benches/basic_bench.rs
+++ benches/basic_bench.rs
@@ -5,9 +5,8 @@ extern crate rand;
 extern crate test;
 
 use intmap::IntMap;
-use intmap::IntMap2;
 use std::collections::HashMap;
-use std::collections::BTreeMap;
+
 
 #[cfg(test)]
 mod tests {
@@ -39,16 +38,15 @@ mod tests {
         let mut map: HashMap<&u64, &u64>  = HashMap::with_capacity(data.len());
 
         for s in data.iter() {
-            test::black_box({
-                map.insert(s, s);
-            });
+            test::black_box(map.insert(s, s)
+            );
         }
 
         b.iter(|| {
             for s in data.iter() {
                 test::black_box({
                     map.contains_key(s);
-                    
+
                 });
             }
         });
@@ -57,13 +55,13 @@ mod tests {
     #[bench]
     fn u64_insert_intmap(b: &mut Bencher) {
         let data = get_random_range(VEC_COUNT);
-        
+
         let mut map = IntMap::with_capacity(data.len());
 
         b.iter(|| {
             map.clear();
             for s in data.iter() {
-                test::black_box(map.insert(*s, s));
+                test::black_box({map.insert(*s, s)});
             }
         });
     }
@@ -72,103 +70,15 @@ mod tests {
     #[bench]
     fn u64_get_intmap(b: &mut Bencher) {
         let data = get_random_range(VEC_COUNT);
-        
+
         let mut map = IntMap::with_capacity(data.len());
-        for s in data.iter() {            
+        for s in data.iter() {
             map.insert(*s, s);
         }
 
         b.iter(|| {
             for s in data.iter() {
-                test::black_box({
-                    map.contains_key(*s);
-                    
-                });
-            }
-        });
-    }
-
-
-    #[bench]
-    fn u64_insert_intmap2(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        
-        let mut map = IntMap2::with_capacity(data.len());
-        
-        let mut cols = map.collisions();
-        let mut vec = Vec::new();
-
-        for c in cols.iter() {
-            vec.push(c.clone());
-        
-        }
-        vec.sort();
-        println!("Collisions: {}", vec.len());
-        for v in vec.iter() {
-            println!("col: {:?}", v);
-        }
-
-
-        b.iter(|| {
-            map.clear();
-            for s in data.iter() {
-                test::black_box(map.insert(*s, s));
-            }
-        });
-    }
-
-
-    #[bench]
-    fn u64_get_intmap2(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        
-        let mut map = IntMap2::with_capacity(data.len());
-        for s in data.iter() {            
-            map.insert(*s, s);
-        }
-
-        b.iter(|| {
-            for s in data.iter() {
-                test::black_box({
-                    map.contains_key(*s);
-                    
-                });
-            }
-        });
-    }
-
-
-
-        #[bench]
-    fn u64_insert_btree(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        
-        let mut map = BTreeMap::new();
-
-        b.iter(|| {
-            map.clear();
-            for s in data.iter() {
-                test::black_box(map.insert(s, s));
-            }
-        });
-    }
-
-
-    #[bench]
-    fn u64_get_btree(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        
-        let mut map = BTreeMap::new();
-        for s in data.iter() {            
-            map.insert(s, s);
-        }
-
-        b.iter(|| {
-            for s in data.iter() {
-                test::black_box({
-                    map.contains_key(s);
-                    
-                });
+                test::black_box(map.contains_key(*s));
             }
         });
     }
@@ -176,7 +86,7 @@ mod tests {
 
     fn get_random_range(count: usize) -> Vec<u64> {
         use rand::{Rng, SeedableRng, StdRng};
-        
+
         let mut vec = Vec::new();
 
         let seed: &[_] = &[4, 2, 4, 2];
@@ -188,7 +98,7 @@ mod tests {
 
         vec.sort();
         vec.dedup();
-        
-        vec    
+
+        vec
     }
 }
